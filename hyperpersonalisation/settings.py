16df073909905de2ea6fc19119725281ab2241fe
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-25 23:35:25 +0530

    configurated simplejwt

--- hyperpersonalisation/settings.py
+++ hyperpersonalisation/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #updating the custom user model
 AUTH_USER_MODEL = "users.User"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
