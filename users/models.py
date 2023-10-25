79a989543ec2a274396a465f9560f3aee7348ccb
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-25 23:48:45 +0530

    added migration and a small bug fix

--- users/models.py
+++ users/models.py
@@ -71,3 +71,10 @@ class User(AbstractBaseUser,PermissionsMixin):
 
   def __str__(self):
       return self.name
+
+
+  def has_perm(self, perm, obj = None):
+      return self.is_admin
+
+  def has_module_perms(self, app_label):
+      return True
