2effdc7b4252671aa9cd5caa4364f4ee87ecb121
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-25 23:11:37 +0530

    feature: added custom base user

---
+++ users/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager,PermissionsMixin
+import uuid
+
+
+class UserManager(BaseUserManager):
+  """
+  Custom Django User Manager for the Custom User Model Created.
+  """
+  def create_user(self, email, name, phone,password=None, *args, **kwargs):
+      user = self.model(
+          email = self.normalize_email(email),
+          name = name,
+          phone = phone,
+          *args, **kwargs
+      )
+      user.set_password(password)
+      user.save(using = self._db)
+      return user
+
+  def create_superuser(self, email,name,phone,password=None, *args, **kwargs):
+      user = self.create_user(
+          email=email,
+          password=password,
+          name = name,
+          phone = phone,
+          *args, **kwargs
+      )
+      user.is_admin = True
+      user.is_staff = True
+      user.save(using=self._db)
+      return user
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+  """
+  Custom Django User Model without Username.
+  """
+
+  ACCOUNT_TYPES = [
+      ('SAV', 'Savings'),
+      ('CHK', 'Checking'),
+      ('BUS', 'Business'),
+      ('JNT', 'Joint'),
+  ]
+
+
+  id = models.CharField(max_length=200, default=uuid.uuid4,unique=True,primary_key=True)
+  email = models.EmailField(null=False, max_length=100,unique=True)
+  name = models.CharField(null=False, max_length=100)
+  phone = models.IntegerField(null=False,unique=True)
+  date_of_birth = models.DateField()
+  account_number = models.CharField(max_length=20, unique=True)
+  account_type = models.CharField(max_length=3, choices=ACCOUNT_TYPES)
+  balance = models.DecimalField(max_digits=10, decimal_places=2)
+  overdraft_limit = models.DecimalField(max_digits=10, decimal_places=2)
+  date_opened = models.DateField()
+  branch_number = models.CharField(max_length=20)
+  created_at = models.DateTimeField(auto_now=True)
+
+
+  is_admin = models.BooleanField(default = False)
+  is_active = models.BooleanField(default = True)
+  is_staff = models.BooleanField(default = False)
+  is_superuser = models.BooleanField(default = False)
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['name','phone', 'date_of_birth', 'account_number', 'account_type', 'balance', 'overdraft_limit', 'date_opened', 'branch_number']
+
+  objects = UserManager()
+
+  def __str__(self):
+      return self.name
