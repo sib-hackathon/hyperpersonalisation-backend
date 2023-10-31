147a97b802212c15cf6ec652d20e9a886238b191
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-31 17:18:03 +0530

    serializer for user button interaction

---
+++ suggestions/serializer.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class ButtonSerializer(serializers.serializer):
+    button_id = serializers.IntegerField(source='button__id')
+    button_name = serializers.CharField(source='button__name')
+    count = serializers.IntegerField()
